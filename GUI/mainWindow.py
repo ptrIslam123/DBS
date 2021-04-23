d159ca99c989e76e06d6fb173080514f41ebc45d
ptrIslam123 <asker.novikov@bk.ru>
2021-04-23 13:12:29 +0300

    create support window for mainWindow {windowEditConf and windowNetSettings}

--- GUI/mainWindow.py
+++ GUI/mainWindow.py
@@ -19,6 +19,9 @@ class MainWindow(QtGui.QMainWindow):
         self.__width = width
         self.__hight = hight
 
+        self.__editConfWindow = None
+        self.__netSettingsWindow = None
+
         self.initUI()
         self.createMenu()
 
@@ -38,12 +41,13 @@ class MainWindow(QtGui.QMainWindow):
     def createMenu(self):
         
         self.__action_edit_conf     = QtGui.QAction("edit config", self)
-        self.__action_run_scripts   = QtGui.QAction("run scripts", self)
-
+        self.__action_run_scripts   = QtGui.QAction("run this current configure", self)
+        self.__action_get_settings_and_run_scripts = QtGui.QAction("get settings and run", self) 
 
         self.__action_edit_conf.triggered.connect(self.edit_conf)
         self.__action_run_scripts.triggered.connect(self.run_scripts)
-
+        self.__action_get_settings_and_run_scripts.triggered.connect(self.get_settings_and_run_scripts)
+        
         
         self.__menubar              = self.menuBar()
 
@@ -54,17 +58,27 @@ class MainWindow(QtGui.QMainWindow):
 
         self.__edit_conf.addAction(self.__action_edit_conf)
         self.__run_scripts.addAction(self.__action_run_scripts)
+        self.__run_scripts.addAction(self.__action_get_settings_and_run_scripts)
+        
 
 
 
     def edit_conf(self):
-        pass
+        if self.__editConfWindow is None:
+            self.__editConfWindow = windows.WindowEditConf("the edit window", 300, 300)
+        
+        self.__editConfWindow.show_window()
 
 
     def run_scripts(self):
         pass
 
 
+    def get_settings_and_run_scripts(self):
+        if self.__netSettingsWindow is None:
+            self.__netSettingsWindow = windows.WindowNetSettings("the net settings window", 300, 300)
+
+        self.__netSettingsWindow.show_window()
 
     def show_window(self):
         self.show()
--- GUI/windows.py
+++ GUI/windows.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from PySide import QtGui
+from vars import *
 
 class BaseWindow(QtGui.QWidget, object):
 
@@ -30,3 +31,153 @@ class BaseWindow(QtGui.QWidget, object):
 
     def show_window(self):
         self.show()
+
+
+    
+
+class WindowEditConf(BaseWindow):
+    
+    def __init__(self, titile, pos_x, pos_y, \
+            width=DEFAULT_M_WINDOW_WIDTH, hight=DEFAULT_M_WINDOW_HIGHT):
+        super(WindowEditConf, self).__init__(titile, pos_x, pos_y, width, hight)
+        
+        self.__resultFileName   =  None
+        self.__emailTo          = None
+        self.__scripts          = None
+        self.__password         = None
+        self.__dataFileName     = None
+        self.__emailFrom        = None
+
+        self.createEditDialog()
+
+
+    def createEditDialog(self):
+        self.__gridBox  = QtGui.QGridLayout()
+
+        self.__reconf   = QtGui.QPushButton("reconfigure")
+
+        resultFileNameLabel = QtGui.QLabel("result file name")
+        emailToLabel        = QtGui.QLabel("email to")
+        scriptsLabel        = QtGui.QLabel("scripts")
+        passwordLabel       = QtGui.QLabel("password")
+        dataFileNameLabel   = QtGui.QLabel("target file name")
+        emailFromLabel      = QtGui.QLabel("emaill from")
+
+
+        self.__resultFileNameLineEdit   = QtGui.QLineEdit()
+        self.__emaillToLineEdit         = QtGui.QLineEdit()
+        self.__scriptsLineEdit          = QtGui.QLineEdit()
+        self.__passwordLineEdit         = QtGui.QLineEdit()
+        self.__dataFileNameLineEdit     = QtGui.QLineEdit()
+        self.__emailFromLineEdit        = QtGui.QLineEdit()
+
+
+        self.__gridBox.addWidget(resultFileNameLabel, 1, 1)
+        self.__gridBox.addWidget(emailToLabel, 2, 1)
+        self.__gridBox.addWidget(scriptsLabel, 3, 1)
+        self.__gridBox.addWidget(passwordLabel, 4, 1)
+        self.__gridBox.addWidget(dataFileNameLabel, 5, 1)
+        self.__gridBox.addWidget(emailFromLabel, 6, 1)
+
+
+        self.__gridBox.addWidget(self.__resultFileNameLineEdit, 1, 2)
+        self.__gridBox.addWidget(self.__emaillToLineEdit, 2, 2)
+        self.__gridBox.addWidget(self.__scriptsLineEdit, 3, 2)
+        self.__gridBox.addWidget(self.__passwordLineEdit, 4, 2)
+        self.__gridBox.addWidget(self.__dataFileNameLineEdit, 5, 2)
+        self.__gridBox.addWidget(self.__emailFromLineEdit, 6, 2)
+        self.__gridBox.addWidget(self.__reconf, 7, 2)
+
+
+        self.setLayout(self.__gridBox)
+
+
+        self.__reconf.clicked.connect(self.reconfigure)
+
+
+
+    def reconfigure(self):
+        self.__resultFileName   = self.__resultFileNameLineEdit.text()
+        self.__emailTo          = self.__emaillToLineEdit.text()
+        self.__scripts          = self.__scriptsLineEdit.text()
+        self.__password         = self.__passwordLineEdit.text()
+        self.__dataFileName     = self.__dataFileNameLineEdit.text()
+        self.__emailFrom        = self.__emailFromLineEdit.text()
+
+
+
+    def get_result_file_name(self):
+        return self.__resultFileName
+
+
+    def get_email_to(self):
+        return self.__emailTo
+
+
+    def get_scripts(self):
+        return self.__scripts
+
+
+    def get_password(self):
+        return self.__password
+
+
+    def get_data_file_name(self):
+        return self.__dataFileName
+
+
+    def get_email_from(self):
+        return self.__emailFrom
+
+
+
+
+
+class WindowNetSettings(BaseWindow):
+
+    def __init__(self, titile, pos_x, pos_y, \
+            width=DEFAULT_M_WINDOW_WIDTH, hight=DEFAULT_M_WINDOW_HIGHT):
+        super(WindowNetSettings, self).__init__(titile, pos_x, pos_y, width, hight)
+        
+        self.__email_addr   = None
+        self.__password     = None
+
+        self.createNetSettingsDialog()
+
+
+    def createNetSettingsDialog(self):
+        self.__gridBox  = QtGui.QGridLayout()
+
+        self.__applyBtn = QtGui.QPushButton("apply")
+
+        emailAddrLabel  = QtGui.QLabel("email address")
+        passwordLabel   = QtGui.QLabel("password")
+
+        self.__emailAddrLineEdit = QtGui.QLineEdit()
+        self.__passwordLineEdit = QtGui.QLineEdit()
+
+
+        self.__gridBox.addWidget(emailAddrLabel, 1, 1)
+        self.__gridBox.addWidget(passwordLabel, 2, 1)
+        self.__gridBox.addWidget(self.__emailAddrLineEdit, 1, 2)
+        self.__gridBox.addWidget(self.__passwordLineEdit, 2, 2)
+        self.__gridBox.addWidget(self.__applyBtn, 3, 2)
+
+        self.setLayout(self.__gridBox)
+
+        self.__applyBtn.clicked.connect(self.applyConf)
+
+
+    def applyConf(self):
+        self.__email_addr   = self.__emailAddrLineEdit.text()
+        self.__password     = self.__passwordLineEdit.text()
+        
+
+
+    def get_email_addr(self):
+        return self.__email_addr
+
+
+    def get_password(self):
+        return self.__password
+        
